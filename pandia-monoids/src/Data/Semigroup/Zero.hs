28f092c17b434d78910070bd9e96d013b25c8ff2
Alice Rixte <alice.rixte@gmail.com>
2025-02-18 16:31:30 +0100

    Monoids  : Left and right zero monoids

--- pandia-monoids/src/Data/Semigroup/Zero.hs
+++ pandia-monoids/src/Data/Semigroup/Zero.hs
@@ -2,3 +2,25 @@ module Data.Semigroup.Zero
   ( module Data.Semigroup.Zero
   ) where
 
+-- | Semigroup with a zero element.
+--
+-- Instances must satisfy the following law :
+--
+-- @ 'zero' '<>' a = zero = a '<>' 'zero' @
+class Semigroup a => HasZero a where
+  -- | The zero element of a semigroup
+  zero :: a
+
+newtype LeftZero a = LeftZero a
+  deriving ( Show, Eq, Ord, Num, Fractional
+           , Floating, Real, RealFrac, RealFloat, Bounded, Enum)
+
+instance Semigroup (LeftZero a) where
+  LeftZero a <> _ = LeftZero a
+
+newtype RightZero a = RightZero a
+  deriving ( Show, Eq, Ord, Num, Fractional
+           , Floating, Real, RealFrac, RealFloat, Bounded, Enum)
+
+instance Semigroup (RightZero a) where
+  _ <> RightZero a = RightZero a
