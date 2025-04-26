6b87012bf0cd0f36c4d3b25864f5650158d6db45
Alice Rixte <alice.rixte@gmail.com>
2025-04-26 15:21:44 +0200

    Lazy right semidirect products

--- pandia-monoids/src/Data/Semigroup/Semidirect/Lazy.hs
+++ pandia-monoids/src/Data/Semigroup/Semidirect/Lazy.hs
@@ -15,77 +15,107 @@
 
 module Data.Semigroup.Semidirect.Lazy
        ( LSemidirect (..)
-       , lactor
-       , lactee
        , lerase
-       , lreset
+       , lforget
        , lfromActee
        , lfromActor
        , lfromPair
+        , RSemidirect (..)
+        , rerase
+        , rforget
+        , rfromActee
+        , rfromActor
+        , rfromPair
        ) where
 
 import Data.Bifunctor
 import Data.Act
 
-import Data.Coerce
-
--- import Data.Aeson
-
 -- | A semi-direct product for a left action, where @s@ acts on @x@
 --
-newtype LSemidirect x s = LSemidirect {lunsemidirect :: (x,s)}
-  deriving (Show, Read, Eq) --, ToJSON, FromJSON)
+data LSemidirect x s = LSemidirect
+  { lactee :: x -- ^ The value being acted on
+  , lactor :: s -- ^ The acting element
+  }
+  deriving (Show, Read, Eq)
 
-instance (Semigroup s, Semigroup x, LAct x s)
+instance LActSgMorph x s
   => Semigroup (LSemidirect x s) where
-  ~(LSemidirect (n1,h1)) <> ~(LSemidirect (n2,h2)) =
-    LSemidirect  (n1 <> (h1 <>$ n2), h1 <> h2)
+  ~(LSemidirect x s) <> ~(LSemidirect x' s') =
+    LSemidirect  (x <> (s <>$ x')) (s <> s')
 
-
-instance (Monoid s, Monoid x, LAct x s) => Monoid (LSemidirect x s) where
-  mempty = LSemidirect (mempty :: x , mempty :: s)
-
+instance LActMnMorph x s => Monoid (LSemidirect x s) where
+  mempty = LSemidirect mempty mempty
 
 instance Functor (LSemidirect x) where
-  fmap :: forall a b. (a -> b) -> LSemidirect x a -> LSemidirect x b
-  fmap = coerce (fmap :: (a -> b) -> (x,a) -> (x,b))
+  fmap f a = a {lactor = f (lactor a)}
 
 instance Bifunctor LSemidirect where
-  bimap :: forall a b c d.
-    (a -> b) -> (c -> d) -> LSemidirect a c -> LSemidirect b d
-  bimap = coerce (bimap :: (a -> b) -> (c -> d) -> (a,c) -> (b,d))
+  first f a = a {lactee = f (lactee a)}
+  second = fmap
 
--- | Get the value being acted on of a semidirect pair
-lactee :: forall x s. LSemidirect x s -> x
-lactee = coerce (fst :: (x,s) -> x)
-
--- | Get the acting element of a semidirect pair
-lactor :: forall x s. LSemidirect x s -> s
-lactor = coerce (snd :: (x,s) -> s)
-
--- |  Erases the element being acted on.
+-- |  Erases the element being acted on (i.e. replace it with @mempty@).
 lerase :: Monoid x => LSemidirect x s -> LSemidirect x s
-lerase (LSemidirect (_,s)) = LSemidirect (mempty,s)
+lerase a = a {lactee = mempty}
 
+-- |  Forget the acting element (i.e. replace it with @mempty@).
+lforget :: Monoid s => LSemidirect x s -> LSemidirect x s
+lforget a =a {lactor = mempty}
 
--- |  Resets the acting element.
---
---  See the article "Unified media programming: An algebraic approach" by
---  S.Archipoff and D. Janin to understand why this function is called reset.
---
-lreset :: Monoid s => LSemidirect x s -> LSemidirect x s
-lreset (LSemidirect (x,_)) = LSemidirect (x,mempty)
-
-
--- |  Creates a pair out of an acting element
+-- |  Make a semidirect pair out whose actee element is @mempty@.
 lfromActor :: Monoid x => s -> LSemidirect x s
-lfromActor s = LSemidirect (mempty,s)
+lfromActor s = LSemidirect mempty s
 
--- |  Creates a pair out of an element being acted on
+-- |  Make a semidirect pair out whose actor element is @mempty@ .
 lfromActee :: Monoid s => x -> LSemidirect x s
-lfromActee x = LSemidirect (x,mempty)
-
+lfromActee x = LSemidirect x mempty
 
 -- | Converts a pair into a semidirect product element
 lfromPair :: (x,s) -> LSemidirect x s
-lfromPair = coerce
+lfromPair (x,s) = LSemidirect x s
+
+
+------------------------------------------------------------------------------
+
+-- |  A semidirect product for a right action, where @s@ acts on @x@
+--
+data RSemidirect x s = RSemidirect
+  { ractee :: x -- ^ The value being acted on
+  , ractor :: s -- ^ The acting element
+  }
+  deriving (Show, Read, Eq)
+
+instance RActSgMorph x s
+  => Semigroup (RSemidirect x s) where
+  ~(RSemidirect x s) <> ~(RSemidirect x' s') =
+    RSemidirect  (x <> (x' $<> s)) (s <> s')
+
+instance RActMnMorph x s => Monoid (RSemidirect x s) where
+  mempty = RSemidirect mempty mempty
+
+instance Functor (RSemidirect x) where
+  fmap f a = a {ractor = f (ractor a)}
+
+instance Bifunctor RSemidirect where
+  first f a = a {ractee = f (ractee a)}
+  second = fmap
+
+-- |  Erases the element being acted on (i.e. replace it with @mempty@).
+rerase :: Monoid x => RSemidirect x s -> RSemidirect x s
+rerase a = a {ractee = mempty}
+
+-- |  Forget the acting element (i.e. replace it with @mempty@).
+rforget :: Monoid s => RSemidirect x s -> RSemidirect x s
+rforget a = a {ractor = mempty}
+
+-- |  Make a semidirect pair out whose actee element is @mempty@.
+rfromActor :: Monoid x => s -> RSemidirect x s
+rfromActor s = RSemidirect mempty s
+
+-- |  Make a semidirect pair out whose actor element is @mempty@ .
+rfromActee :: Monoid s => x -> RSemidirect x s
+rfromActee x = RSemidirect x mempty
+
+-- | Converts a pair into a semidirect product element
+rfromPair :: (x,s) -> RSemidirect x s
+rfromPair (x,s) = RSemidirect x s
