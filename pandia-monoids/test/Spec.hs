d4ed5796b25944086da02775a74176e7399ed2c0
Alice Rixte <alice.rixte@gmail.com>
2025-05-09 20:28:47 +0200

    Strict Semidirect products

--- pandia-monoids/test/Spec.hs
+++ pandia-monoids/test/Spec.hs
@@ -9,19 +9,58 @@ import Test.QuickCheck
 import Data.Monoid
 import Data.Act
 
+import qualified Data.Semidirect.Lazy as Lazy
+import qualified Data.Semidirect.Strict as Strict
+
 main :: IO ()
 main = hspec $ do
+  describe "Semidirect" $ do
+    describe "LSemidirect" $ do
+      describe "Lazy" $ do
+        it "Product on Sum Semigroup" $ property $
+          \x s y t ->
+            Lazy.LSemidirect (Sum (x :: Int)) (Product (s :: Int))
+            <> Lazy.LSemidirect (Sum y) (Product t)
+            `shouldBe`
+            Lazy.LSemidirect (Sum (x + s*y)) (Product (s*t))
+        it "Product on Sum Monoid" $
+          mempty `shouldBe`
+            Lazy.LSemidirect (mempty :: Sum Int) (mempty :: Product Int)
+      describe "Strict" $ do
+        it "Product on Sum Semigroup" $ property $
+          \x s y t ->
+            Strict.LSemidirect (Sum (x :: Int)) (Product (s :: Int))
+            <> Strict.LSemidirect (Sum y) (Product t)
+            `shouldBe`
+            Strict.LSemidirect (Sum (x + s*y)) (Product (s*t))
+        it "Product on Sum Monoid" $
+          mempty `shouldBe`
+            Strict.LSemidirect (mempty :: Sum Int) (mempty :: Product Int)
+    describe "RSemidirect" $ do
+      describe "Lazy" $ do
+        it "Product on Sum Semigroup" $ property $
+          \x s y t ->
+            Lazy.RSemidirect (Sum (x :: Int)) (Product (s :: Int))
+            <> Lazy.RSemidirect (Sum y) (Product t)
+            `shouldBe`
+            Lazy.RSemidirect (Sum (x + s*y)) (Product (s*t))
+        it "Product on Sum Monoid" $
+          mempty `shouldBe`
+            Lazy.RSemidirect (mempty :: Sum Int) (mempty :: Product Int)
+      describe "Strict" $ do
+        it "Product on Sum Semigroup" $ property $
+          \x s y t ->
+            Strict.RSemidirect (Sum (x :: Int)) (Product (s :: Int))
+            <> Strict.RSemidirect (Sum y) (Product t)
+            `shouldBe`
+            Strict.RSemidirect (Sum (x + s*y)) (Product (s*t))
+        it "Product on Sum Monoid" $
+          mempty `shouldBe`
+            Strict.RSemidirect (mempty :: Sum Int) (mempty :: Product Int)
+
   describe "Action" $ do
     describe "ActSelf" $ do
       it "Int acts on unit" $ property $
         \x -> (x :: Int) <>$ () `shouldBe` ()
       it "Unit acts on char" $ property $
         \x -> () <>$ (x :: Char) `shouldBe` x
-
--- haha = Sum (1::Int) <>$ [Sum (2::Int)]
--- y0 = [ActSelf [Sum (1::Int)], ActSelf [Sum (1::Int)]] <>$  [Sum (2::Int)]
--- y2 = ActSelf [[Sum (1::Int)], [Sum (1::Int)]] <>$  [[Sum (2::Int)]]
--- y3 =  [[Sum (1::Int)], [Sum (1::Int)]] <>$ [[Sum (2::Int)]]
--- y1 = [[Sum (1::Int)]] <>$  [Sum (2::Int)]
-
--- y5 = ActMap (ActSelf (Sum (1 :: Int))) <>$ (Sum 2 :: Sum Int, Sum 3 :: Sum Int)
